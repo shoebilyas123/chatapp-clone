a3d5fccc596618e0552831a96da9e1c18b8c31f0
Shoeb Ilyas <shoebilyas123@gmail.com>
2022-04-30 21:28:31 +0530

    test branch

--- client/src/Components/Contacts/index.tsx
+++ client/src/Components/Contacts/index.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Dropdown, ListGroup } from "react-bootstrap";
 import { IFRRequests } from "../../Interface/redux";
-import DefaultAvatar from "../DefaultAvatar";
-import DropdownToggle from "react-bootstrap/esm/DropdownToggle";
 import ContactItem from "./ContactItem";
 interface IProps {
   contacts: IFRRequests[];
@@ -12,7 +10,7 @@ const Contact: React.FC<IProps> = ({ contacts }) => {
   return (
     <ListGroup className="m-2">
       {contacts.map((ct) => (
-        <ListGroup.Item className="d-flex align-items-center justify-content-between shadow-sm">
+        <ListGroup.Item className="d-flex align-items-center justify-content-between">
           <ContactItem friend={ct} />
         </ListGroup.Item>
       ))}
